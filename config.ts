5fe3434d8e095d2515d3609ac0e192b4f0434b57
Indrani123 <isenmpccet@gmail.com>
2016-10-25 13:18:27 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,13 +1,8 @@
 /**
  * Created by IndraniS on 10/20/2016.
  */
-import {ISigmaIdpOptions} from "./auth/ISigmaIdpOptions";
-export let sigmaIdpOpts:ISigmaIdpOptions = {
-    protocol:"http",
-    host:"localhost",
-    port:8090
 
-};
+
 
 export let daoConfig:any={
    host: "localhost",
